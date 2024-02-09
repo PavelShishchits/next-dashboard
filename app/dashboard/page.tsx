9520f53d3e41e8722345df80a97925a52edd62c8
pavel s <p.shischits@ext.kuehlhaus.com>
2024-02-09 09:56:37 +0100

    add search + pagination

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -2,7 +2,6 @@ import { Card } from '@/app/ui/dashboard/cards';
 import RevenueChart from '@/app/ui/dashboard/revenue-chart';
 import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
 import CardsWrapper from '@/app/ui/dashboard/cards';
-// toDo move to api routes https://nextjs.org/docs/app/building-your-application/routing/route-handlers
 import { Suspense } from 'react';
 import {
   RevenueChartSkeleton,
@@ -13,8 +12,6 @@ import {
 export const dynamic = 'force-dynamic';
 
 export default async function Page() {
-  // const [cardData] = await Promise.all([fetchCardData()]);
-
   return (
     <main>
       <h1 className={`mb-4 text-xl md:text-2xl`}>Dashboard</h1>
@@ -22,26 +19,6 @@ export default async function Page() {
         <Suspense fallback={<CardsSkeleton />}>
           <CardsWrapper />
         </Suspense>
-        {/* <Card
-          title="Collected"
-          value={cardData?.totalPaidInvoices}
-          type="collected"
-        />
-        <Card
-          title="Pending"
-          value={cardData?.totalPendingInvoices}
-          type="pending"
-        />
-        <Card
-          title="Total Invoices"
-          value={cardData?.numberOfInvoices}
-          type="invoices"
-        />
-        <Card
-          title="Total Customers"
-          value={cardData?.numberOfCustomers}
-          type="customers"
-        /> */}
       </div>
       <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
         <Suspense fallback={<RevenueChartSkeleton />}>
